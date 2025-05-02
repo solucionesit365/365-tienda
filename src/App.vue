e6733cd806db1f8f6eb77c920abb211942097885
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2025-05-02 11:04:26 +0200

    Base

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Hey</h1>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/firebase/index.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCME2CYaIso7PngM2wihV4LZGB_JzZoKjc",
+  authDomain: "silema.firebaseapp.com",
+  projectId: "silema",
+  storageBucket: "silema.appspot.com",
+  messagingSenderId: "565898129046",
+  appId: "1:565898129046:web:7245c59623deb1f72c1cb7",
+  measurementId: "G-1J6JS28GMD",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);

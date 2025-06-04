ad6e654d21f26243adad62497f3dd3cde5eef153
Ismael <imariduena@365obrador.com>
2025-06-04 13:12:16 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import { RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
-import BackButton from "./components/BackButton.vue";
-</script>
-
 <template>
   <div class="container">
     <BackButton />
